16b7810295b97a8e3455a4cd1ad70a6c83f62e6d
김종수 (주차영상APP/SW셀) <1625289@mobis.co.kr>
2023-02-27 08:12:00 +0000

    Day 01

---
+++ 01_Saying_Hello.py
@@ -0,0 +1,17 @@
+# name = input("What is your name? ")
+# print(f"Hello, {name}, nice to meet you!")
+
+""" 
+Challenge 1
+Write a new version of the program without using any variables.
+"""
+print("Hello, {a}, nice to meet you!".format(a = input("What is your name? ")))
+
+"""
+Challenge 2
+Write a version of the program that displays different
+greetings for different people. This would be a good
+challenge to try after you’ve completed the exercises in
+Chapter 4, Making Decisions, on page 29 and Chapter 7,
+Data Structures, on page 63
+"""
