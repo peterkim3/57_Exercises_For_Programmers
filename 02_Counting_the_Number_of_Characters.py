6bfcf4ac2df474c02a798d5cb5915344aa7d8b0d
peterkim3 <hanskjs9@knou.ac.kr>
2023-02-28 00:17:51 +0000

    Day 2 - Challenge not finished yet

---
+++ 02_Counting_the_Number_of_Characters.py
@@ -0,0 +1,16 @@
+inputString = input("What is the input string? ")
+if not len(inputString) == 0:
+    print(f"{inputString} has {len(inputString)} characters")
+else:
+    print(f"Please enter something into the program!")
+    
+    
+"""
+Challenges
+Implement this program using a graphical user interface
+and update the character counter every time a key is
+pressed. If your language doesn’t have a particularly
+friendly GUI library, try doing this exercise with HTML
+and JavaScript instead.
+"""
+
